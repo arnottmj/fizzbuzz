1f78828f7c01fff185ac78e76c885198d0825c31
Michael Arnott <arnottmj@aol.com>
2015-06-09 11:17:40 +0100

    further updates

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,6 +7,18 @@ describe 'fizzbuzz' do
   it 'return "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'return number when passed 2' do
+    expect(fizzbuzz(2)).to eq 2 
+  end
+
+
+
 
 end
 
