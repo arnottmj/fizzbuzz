abe5159f9508a1bea8cf7e0d63def6c402886f40
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-09 11:54:40 +0100

    first step refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,20 @@
-def fizzbuzz(number)
-  return 'fizzbuzz' if number % 15 == 0
-  return 'fizz' if number % 3 == 0
-  return 'buzz' if number % 5 == 0
-  number
+
+def is_divisible_by_3? (number)
+  number % 3 == 0
 end
 
+def is_divisible_by_5? (number)
+  number % 5 == 0
+end
+
+def is_divisible_by_15? (number)
+  number % 15 == 0
+end
+
+
+def fizzbuzz(number)
+  return 'fizzbuzz' if is_divisible_by_15? (number)
+  return 'fizz' if is_divisible_by_3?(number)
+  return 'buzz' if is_divisible_by_5?(number)
+  number
+end
